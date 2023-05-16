9f498120005701864d58c6adbb5847af036b8217
Fa-BRAIK <faroukbraik17@gmail.com>
2023-05-16 19:07:51 -0400

    [dev] advancement

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PeopleController;
+use App\Http\Controllers\PlanetController;
+use App\Http\Controllers\StarshipController;
+use App\Http\Controllers\ResourceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('people')->group(static function () {
+    // Option B : search included in index by passing a query param ?search=luke
+    Route::get('/', [ PeopleController::class, 'index' ]);
+
+    // Option A : search seperated from index
+    Route::get('/search', [ PeopleController::class, 'search' ]); 
 });
+
+Route::get('planets', [ PlanetController::class, 'index' ]);
+Route::get('starships', [ StarshipController::class, 'index' ]);
+
+// Option C : include all routes in one file by using a variable to determine the resource
+// we can also pass a query param ?search=luke in this case
+Route::get('/{resource}', [ ResourceController::class, 'index' ]);
